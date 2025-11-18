bcf14e7d16430b5d9caec1d5ce824c40b0d735bd
simonkuhlo <siga241@gmail.com>
2025-11-18 01:26:52 +0100

    replace old plugin structure

--- WebInterface/plugin.py
+++ WebInterface/plugin.py
@@ -1,37 +1,32 @@
-from SimonsPluginResources.environment import Environment
-from SimonsPluginResources.plugin import Plugin
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from SimonsPluginResources.plugin_host import HostPlugin
-from SimonsPluginResources.plugin_request import PluginRequest
-from SimonsPluginResources.settings.models.scope import ScopePlugin
+from typing import Optional, TYPE_CHECKING
+from SimonsPluginResources.asyncio_task_wrapper import AsyncTask
+from SimonsPluginResources.plugin import Plugin, PluginMeta
 from SimonsPluginResources.settings import Setting
-from .main import WebInterfacePluginExtension
+from SimonsPluginResources.settings.models.scope import ScopePlugin
+from . import main
+if TYPE_CHECKING:
+    from SimonsPluginResources.plugin_host import PluginHost
 
-class WebInterfacePlugin(Plugin):
-    def __init__(self, environment: Environment):
-        super().__init__(plugin_id = "WEBINTERFACE",
-                         environment = environment,
-                         name = "Webinterface",
-                         description = "Webinterface",
-                         version = 1,
-                         used_host_version = 0,
-                         plugin_connections=[PluginRequest("HOST", 0, True)]
-                         )
-        self.host_plugin: "HostPlugin" = None
-        self.add_plugin_extension(WebInterfacePluginExtension(self))
-
-    def get_settings(self) -> list[Setting]:
-        return [
-            Setting(rel_path="host_address", default_value="localhost", scope=ScopePlugin(plugin_id=self.plugin_id), comment="Webinterface must be restarted after changing value!"),
-            Setting(rel_path="port", default_value="8080", scope=ScopePlugin(plugin_id=self.plugin_id), comment="Webinterface must be restarted after changing value!")
+class WebInterfacePluginMeta(PluginMeta):
+    def __init__(self):
+        super().__init__(plugin_id = "webinterface")
+        self.name = "Webinterface"
+        self.description = "No description provided"
+        self.version = 2
+        self.used_backend_version = 10
+        self.connection_requests = None
+        self.settings = [
+            Setting(rel_path="host_address", default_value="localhost", scope=ScopePlugin(plugin_id=self.plugin_id),
+                    comment="Webinterface must be restarted after changing value!"),
+            Setting(rel_path="port", default_value="8080", scope=ScopePlugin(plugin_id=self.plugin_id),
+                    comment="Webinterface must be restarted after changing value!")
         ]
 
-    def add_plugin_link(self, plugin:"Plugin") -> None:
-        super().add_plugin_link(plugin)
-        if plugin.plugin_id == "HOST":
-            self.host_plugin = plugin
+class WebInterfacePlugin(Plugin):
+    def __init__(self, host: "PluginHost"):
+        super().__init__(host, WebInterfacePluginMeta())
+        self.webinterface = main.WebInterface(self)
 
-def get_plugin(environment: Environment):
-    return WebInterfacePlugin(environment)
+    @property
+    def tasks(self) -> Optional[list[AsyncTask]]:
+        return[AsyncTask(self.webinterface.run_webinterface, name="Webinterface main loop")]
